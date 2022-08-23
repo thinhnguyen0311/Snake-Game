37991ff99315a4a5c4c31249f38818be5e425809
ThinhNguyen0311 <nguyenphuocthinh93@gmail.com>
2022-08-23 22:41:41 +0700

    Create a 1-block snake.

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import pygame
 from pygame.locals import *
 
+
+def draw_block():
+    surface.fill((110, 110, 5))
+    surface.blit(block, (block_x, block_y))
+    pygame.display.flip()
+
+
 if __name__ == "__main__":
     pygame.init()
 
     surface = pygame.display.set_mode((1000, 500))
     surface.fill((110, 110, 5))
+
+    block = pygame.image.load('resources/block.jpg').convert()
+    block_x, block_y = 100, 100
+    surface.blit(block, (block_x, block_y))
+
     pygame.display.flip()
 
     running = True
@@ -14,5 +26,19 @@ if __name__ == "__main__":
             if event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
                     running = False
+
+                if event.key == K_UP:
+                    block_y -= 10
+                    draw_block()
+                elif event.key == K_DOWN:
+                    block_y += 10
+                    draw_block()
+                elif event.key == K_LEFT:
+                    block_x -= 10
+                    draw_block()
+                elif event.key == K_RIGHT:
+                    block_x += 10
+                    draw_block()
+
             elif event.type == QUIT:
                 running = False
