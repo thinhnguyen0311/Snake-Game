f0c2b25e540b7f9204167515356ac75ec7dfdd7d
ThinhNguyen0311 <nguyenphuocthinh93@gmail.com>
2022-09-04 10:50:59 +0700

    Create collision occurence. Increasing the length of the snake whenever it hits the apple. Moving the apple to random position afterwards.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import *
 import time
+import random
 
 
 SIZE = 40
@@ -10,12 +11,16 @@ class Apple:
     def __init__(self, parent_screen):
         self.parent_screen = parent_screen
         self.image = pygame.image.load('resources/apple.jpg').convert()
-        self.x = SIZE * 3
-        self.y = SIZE * 3
+        self.x = random.randrange(0, 20) * SIZE
+        self.y = random.randrange(0, 20) * SIZE
 
     def draw(self):
         self.parent_screen.blit(self.image, (self.x, self.y))
         pygame.display.flip()
+
+    def move(self):
+        self.x = random.randrange(0, 20) * SIZE
+        self.y = random.randrange(0, 20) * SIZE
 
 
 class Snake:
@@ -26,6 +31,11 @@ class Snake:
         self.x = [SIZE]*length
         self.y = [SIZE]*length
         self.direction = 'right'
+
+    def increase_length(self):
+        self.length += 1
+        self.x.append(0)
+        self.y.append(0)
 
     def draw(self):
         self.parent_screen.fill((110, 110, 5))
@@ -65,16 +75,25 @@ class Snake:
 class Game:
     def __init__(self):
         pygame.init()
-        self.surface = pygame.display.set_mode((700, 700))
+        self.surface = pygame.display.set_mode((20*SIZE, 20*SIZE))
         self.surface.fill((110, 110, 5))
         self.snake = Snake(self.surface)
         self.apple = Apple(self.surface)
         self.snake.draw()
         self.apple.draw()
 
+    def is_collision(self, x1, y1, x2, y2):
+        if x1 == x2 and y1 == y2:
+            return True
+        return False
+
     def play(self):
         self.snake.walk()
         self.apple.draw()
+
+        if self.is_collision(self.snake.x[0], self.snake.y[0], self.apple.x, self.apple.y):
+            self.snake.increase_length()
+            self.apple.move()
 
     def run(self):
         running = True
@@ -99,7 +118,7 @@ class Game:
 
             self.play()
 
-            time.sleep(0.3)
+            time.sleep(0.25)
 
 
 if __name__ == "__main__":
